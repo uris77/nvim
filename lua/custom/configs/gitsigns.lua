d992ef59caf1b01b1a66011b79fe2016acdf2507
Roberto Guerra <uris77@gmail.com>
2024-04-06 21:36:05 -0600

    fix gitsigns shortcuts

--- lua/custom/configs/gitsigns.lua
+++ lua/custom/configs/gitsigns.lua
@@ -6,10 +6,10 @@ local opts = {
   vim.keymap.set("n", "<leader>gb", function ()
     gs.blame_line { full = false }
   end),
-  vim.keymap.set("n", "<leader>gd", function ()
+  vim.keymap.set("n", "<leader>gi", function ()
     gs.diffthis()
   end),
-  vim.keymap.set("n", "<leader>gD", function ()
+  vim.keymap.set("n", "<leader>gc", function ()
     gs.diffthis '~'
   end),
 }
