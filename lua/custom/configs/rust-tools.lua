85de84b4a943f3aee76dd43686fd8ade1777ed21
Roberto Guerra <uris77@gmail.com>
2024-04-13 20:59:25 -0600

    fix rust setup

--- lua/custom/configs/rust-tools.lua
+++ lua/custom/configs/rust-tools.lua
@@ -1,7 +1,7 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local options {
+local options = {
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
